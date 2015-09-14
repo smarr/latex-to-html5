feeb07df4f3ae2438b7376a215bbe2ec4633c023
Stefan Marr <git@stefan-marr.de>
2015-09-14 23:14:50 +0200

    Added failure reporting and proper return code
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- run-tests.py
+++ run-tests.py
@@ -7,19 +7,32 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 def exec_test(d):
   os.chdir(d)
-  subprocess.call([BASE_DIR + '/ht-latex', 'test.tex', '.'])
-  subprocess.call(['diff', 'expected.html', 'test-final.html'])
+  if subprocess.call([BASE_DIR + '/ht-latex', 'test.tex', '.']) == 0:
+    if subprocess.call(['diff', 'expected.html', 'test-final.html']) == 0:
+      return True
+  
+  print d, 'FAILED'
+  return False
 
 
 def exec_tests():
   os.chdir(BASE_DIR)
   
+  failed = False
+  
   for f in os.listdir('tests'):
     if os.path.isdir('tests/' + f):
-      exec_test('tests/' + f)
+      if not exec_test('tests/' + f):
+        failed = True
+  return failed
+
 
 if len(sys.argv) > 1:
-  exec_test(sys.argv[1])
+  result = exec_test(sys.argv[1])
 else:
-  exec_tests()
-  
+  result = exec_tests()
+
+if result:
+  sys.exit(0)
+else:
+  sys.exit(1)
