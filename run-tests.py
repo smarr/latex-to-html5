297398da826480283fa88e33e58ed0e14aa4f18e
Stefan Marr <git@stefan-marr.de>
2015-09-14 23:25:09 +0200

    Clean up directory on test success
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- run-tests.py
+++ run-tests.py
@@ -5,10 +5,18 @@ import sys
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
+def cleanup_directory(d):
+  subprocess.call(['rm', '-f', 'test.aux', 'test.css', 'test.fls', 'test.lg',
+                         'test.4ct', 'test.bbl', 'test.dvi', 'test.html',
+                         'test.log', 'test.tmp', 'test.4tc', 'test.blg',
+                         'test.fdb_latexmk', 'test.idv', 'test.pdf',
+                         'test.xref', 'test-final.html', 'test-tidy.html'])
+
 def exec_test(d):
   os.chdir(d)
   if subprocess.call([BASE_DIR + '/ht-latex', 'test.tex', '.']) == 0:
     if subprocess.call(['diff', 'expected.html', 'test-final.html']) == 0:
+      cleanup_directory(d)
       return True
   
   print d, 'FAILED'
