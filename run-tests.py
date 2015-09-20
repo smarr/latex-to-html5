dc2014c178f2c7bd96b24afb24755a9bd07a42ec
Stefan Marr <git@stefan-marr.de>
2015-09-20 11:23:47 +0200

    Make run-tests more robust, and fix indentation
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- run-tests.py
+++ run-tests.py
@@ -5,42 +5,55 @@ import sys
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
+
 def cleanup_directory(d):
-  subprocess.call(['rm', '-f', 'test.aux', 'test.css', 'test.fls', 'test.lg',
-                         'test.4ct', 'test.bbl', 'test.dvi', 'test.html',
-                         'test.log', 'test.tmp', 'test.4tc', 'test.blg',
-                         'test.fdb_latexmk', 'test.idv', 'test.pdf',
-                         'test.xref', 'test-final.html', 'test-tidy.html'])
+    subprocess.call(['rm', '-f', 'test.aux', 'test.css', 'test.fls', 'test.lg',
+                           'test.4ct', 'test.bbl', 'test.dvi', 'test.html',
+                           'test.log', 'test.tmp', 'test.4tc', 'test.blg',
+                           'test.fdb_latexmk', 'test.idv', 'test.pdf',
+                           'test.xref', 'test-final.html', 'test-tidy.html',
+                           '.test.lb'])
+
 
 def exec_test(d):
-  os.chdir(d)
-  if subprocess.call([BASE_DIR + '/ht-latex', 'test.tex', '.']) == 0:
-    if subprocess.call(['diff', 'expected.html', 'test-final.html']) == 0:
-      cleanup_directory(d)
-      return True
-  
-  print d, 'FAILED'
-  return False
+    os.chdir(d)
+    try:
+        subprocess.check_output([BASE_DIR + '/ht-latex', 'test.tex', '.'])
+        try:
+            subprocess.check_output(['diff', 'expected.html', 'test-final.html'])
+        except subprocess.CalledProcessError as e:
+            print d, 'FAILED'
+            print "Diff between expected and actual HTML:"
+            print e.output
+            return False
+
+        cleanup_directory(d)
+        return True
+    except subprocess.CalledProcessError as e:
+        print d, 'FAILED'
+        print "Latex Output:"
+        print e.output
+        return False
 
 
 def exec_tests():
-  os.chdir(BASE_DIR)
+    failed = False
   
-  failed = False
-  
-  for f in os.listdir('tests'):
-    if os.path.isdir('tests/' + f):
-      if not exec_test('tests/' + f):
-        failed = True
-  return failed
+    for f in os.listdir('tests'):
+        os.chdir(BASE_DIR)
+        print "-------", f
+        if os.path.isdir('tests/' + f):
+            if not exec_test('tests/' + f):
+                failed = True
+    return failed
 
 
 if len(sys.argv) > 1:
-  result = exec_test(sys.argv[1])
+    result = exec_test(sys.argv[1])
 else:
-  result = exec_tests()
+    result = exec_tests()
 
 if result:
-  sys.exit(0)
+    sys.exit(0)
 else:
-  sys.exit(1)
+    sys.exit(1)
