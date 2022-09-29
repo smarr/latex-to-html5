19e3298cb73592c06941ae55ca1047642f50965f
Stefan Marr <git@stefan-marr.de>
2022-09-29 21:12:24 +0100

    Fix handling of links to citations
    In the current version of tex4ht there’s a <span class=‘cite’> which breaks some of my clean up.
    
    This change should fix this.
    
    It changes slightly the expectations of the tests, but this should be fine.
    It’s either whitespace, or what looks like improvements/more consistent linking.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- post-process-html.py
+++ post-process-html.py
@@ -8,7 +8,7 @@ soup = BeautifulSoup(open(sys.argv[1]), "html5lib")
 
 
 def combine_citation_links(soup):
-    for e in soup.find_all("a", href=re.compile("^#")):
+    for e in soup.find_all("span", attrs={"class": "cite"}):
         # find_all will create a list, that also contains stuff
         # we already processed, i.e., without parent
         if e.parent is None:
@@ -18,8 +18,12 @@ def combine_citation_links(soup):
             # within a list, use normal spaces to avoid typesetting issues
             e.previous_sibling.replace_with(", ")
 
+        a_elem = e.next_element
+
         # also ignore just year links, we might just have fixed the space before
-        if e.contents[0].isnumeric():
+        if a_elem.contents[0].isnumeric():
+            # though, we should still unwrap them
+            e.replace_with(a_elem)
             continue
 
         # Process text like '<a>Author</a>&nbsp;[<a>Year</a>]'
@@ -27,63 +31,72 @@ def combine_citation_links(soup):
                 e.next_sibling.next_sibling and
                 e.next_sibling.next_sibling.next_sibling):
             space_bracket = e.next_sibling
-            year_node_a   = e.next_sibling.next_sibling
+            year_node_span = e.next_sibling.next_sibling
+            year_node_a = year_node_span.next_element
             following_bracket_and_more = e.next_sibling.next_sibling.next_sibling
 
             if str(following_bracket_and_more)[0] == ']':
-                assert e.attrs['href'] == year_node_a.attrs['href'], "should be always the same, because we want to merge those two <a> tags"
+                assert a_elem.attrs['href'] == year_node_a.attrs['href'],\
+                    "should be always the same, because we want to merge those two <a> tags"
                 year = str(year_node_a.contents[0])
-                e.contents[0].replace_with(e.contents[0] + '\xa0[' + year + "]")
+                a_elem.contents[0].replace_with(a_elem.contents[0] + '\xa0[' + year + "]")
                 space_bracket.extract()
+                year_node_span.extract()
                 year_node_a.extract()
 
                 # remove closing bracket
                 following_bracket_and_more.replace_with(
                     str(following_bracket_and_more)[1:])
+                e.replace_with(a_elem)
                 continue
-            else:
-                # transform 'Author&nbsp;[<a>Year</a>, <a>Year2</a>]'
-                e.replace_with(e.contents[0])  # remove link from author
-                continue
+
+            # transform 'Author&nbsp;[<a>Year</a>, <a>Year2</a>]'
+            e.replace_with(a_elem.contents[0])  # remove link from author
+            continue
 
         # Process text like '[<a>Author</a>,&nbsp;<a>Year</a>]'
         # This also includes situations with multiple references, that are
         # listed with author+year.
         if (e.next_sibling == ',\xa0' and
-                isinstance(e.next_sibling.next_sibling, Tag) and
-                e.next_sibling.next_sibling.name == "a" and
-                e.next_sibling.next_sibling.attrs['href'] == e.attrs['href']):
-            comma_space_node = e.next_sibling
-            year_node_a      = e.next_sibling.next_sibling
-            year_node        = year_node_a.contents[0]
+                isinstance(e.next_sibling.next_sibling, Tag)):
+            possible_span = e.next_sibling.next_sibling
+            if possible_span.name == 'span' and possible_span.attrs.get('class') == ['cite']:
+                a_elem2 = possible_span.next_element
+                if a_elem2.name == 'a' and a_elem2.attrs.get('href') == a_elem.attrs['href']:
+                    comma_space_node = e.next_sibling
+                    year_node_span = e.next_sibling.next_sibling
+                    year_node_a = year_node_span.next_element
+                    year_node = year_node_a.contents[0]
 
-            if (year_node_a.next_sibling and
-                    year_node_a.next_sibling.next_sibling and
-                    year_node_a.next_sibling.next_sibling.name == "a" and
-                    year_node_a.next_sibling.next_sibling.contents[0].isnumeric()):
-                # looks like we got multiple years with the same author in
-                # succession, remove the link on the author to avoid confusion
-                prev = e.previous_sibling
-                if prev == ',\xa0':
-                    prev.replace_with(", ")
-                elif (isinstance(prev, NavigableString) and
-                        str(prev)[-1] == "["):
-                    prev.replace_with(s[:-2] + "\N{THIN SPACE}[")
-                e.replace_with(e.contents[0])
+                    if (year_node_a.next_sibling and
+                            year_node_a.next_sibling.next_sibling and
+                            year_node_a.next_sibling.next_sibling.name == "a" and
+                            year_node_a.next_sibling.next_sibling.contents[0].isnumeric()):
+                        # looks like we got multiple years with the same author in
+                        # succession, remove the link on the author to avoid confusion
+                        prev = e.previous_sibling
+                        if prev == ',\xa0':
+                            prev.replace_with(", ")
+                        elif (isinstance(prev, NavigableString) and
+                              str(prev)[-1] == "["):
+                            prev.replace_with(s[:-2] + "\N{THIN SPACE}[")
+                        e.replace_with(e.contents[0])
 
-            else:
-                comma_space_node.extract()
-                year_node_a.extract()
-                e.append(comma_space_node)
-                e.append(year_node)
+                    else:
+                        comma_space_node.extract()
+                        year_node_span.extract()
+                        a_elem.append(comma_space_node)
+                        a_elem.append(year_node)
 
-                # if we are the first in the list, make sure the bracket is
-                # directly connected with a &thinsp; to the element before
-                if isinstance(e.previous_sibling, NavigableString):
-                    s = str(e.previous_sibling)
-                    if s[-1] == "[" and s[-2].isspace():
-                        e.previous_sibling.replace_with(s[:-2] +
-                                                        "\N{THIN SPACE}[")
+                        # if we are the first in the list, make sure the bracket is
+                        # directly connected with a &thinsp; to the element before
+                        if isinstance(e.previous_sibling, NavigableString):
+                            s = str(e.previous_sibling)
+                            if s[-1] == "[" and s[-2].isspace():
+                                e.previous_sibling.replace_with(s[:-2] + "\N{THIN SPACE}[")
+
+        # at the end, unwrap the <a> from the <span class='cite'>, we don't really need it
+        e.replace_with(a_elem)
 
 
 def remove_font_spans(soup):
